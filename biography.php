c09d2499d9cebf61ba2e1253b75af78359ae2e12
Brad Azevedo <brad2k@gmail.com>
2012-07-05 21:01:31 -0700

    Update copy

--- biography.php
+++ biography.php
@@ -12,12 +12,11 @@ include("header.php");
 
 	<p>Susie Byleckie is an experienced esthetician that has been established in the Sacramento cosmetology community for twenty-five years. As an esthetician, Susie frequently attends classes on skin care and treatment. Of these classes, a five-day medical course on the cause and effect of various skin conditions has been integral in Susie's development as a skincare specialist.</p>
 
-	<p>Susie's clients benefit from her ability to correct and prevent severe dry skin, acne, and oily skin problems.  She approaches each client's specific needs with a tailored skincare program that includes treatments and proper product use.</p>
+	<p>Susie's clients benefit from her ability to correct and prevent severe dry skin, acne, and oily skin problems. In her recent past she has gravitated to mature skin problems. She approaches each client's specific needs with a tailored skincare program that includes treatments and proper product use.</p>
 
-	<p>One of Susie's most positive traits is her bedside manner.  Once Susie has touched a client via consultation or an actual skin experience, she has created a rapport with that person. It is in this way that Susie has created a strong referral business.</p>
+	<p>Susie is proficient in the practices of microdermabrasion, peeling and reflexology. She uses a "sugaring" technique for hair removal that is very effective and is much less invasive than a standard waxing.</p>
 
-	<p>Susie is proficient in the practices of microdermabrasion, peeling and reflexology. She uses a "sugaring" technique for hair removal that is very effective and is much less invasive than a standard waxing.  Also, she offers her clients consultation and application of Jane Iredale Mineral Cosmetics with the intention of creating a healthy, natural look.
-	In her recent past she has gravitated to mature skin problems.</p>
+	<p>One of Susie's most positive traits is her bedside manner. Once Susie has touched a client via consultation or an actual skin experience, she has created a rapport with that person. It is in this way that Susie has created a strong referral business.</p>
 
 	<p>With a strong focus on professionalism and a personal approach to skin care, Susie is an outstanding resource for all of your skincare needs and concerns.</p>
 
